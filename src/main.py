346850dca14407ed17a69302760157b6fa0bbbf6
ravinu <github@swore3556.anonaddy.me>
2024-10-19 07:35:05 +0530

    Add input file parsing functionality

--- src/main.py
+++ src/main.py
@@ -9,3 +9,23 @@ def argParser():
         return None , 1
 
 #file,code = argParser()
+
+#if code == 1:
+#    exit()
+
+def fileParser(fileName):
+    fileContents = []
+    import os
+
+    if os.path.isfile(fileName) == True and open(fileName, 'r').read(1) != '':
+        File = open(fileName, 'r')
+        fileContents = File.read().split('\n')
+        return fileContents , 0
+    else:
+        print("File does not exist or is empty.")
+        return None , 1
+
+#text,code1 = fileParser(file)
+
+#if code1 == 1:
+#    exit()
